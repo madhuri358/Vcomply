61fd9a4fdf4fce5d65704913d8c7e0b282230bef
madhuri358 <76027277+madhuri358@users.noreply.github.com>
2020-12-15 09:55:45 +0530

    Add files via upload

---
+++ freeslots.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// P1
+// [['9:30', '10:30'], ['12:00','14:00'], ['15:30', '17:00'], ['18:00', '18:30']]
+
+// [9:00, 18:30]
+
+// P2
+// [['10:00', '10:30'], ['11:00', '13:00'], ['15:00', '16:30'], ['17:30', '18:00']]
+
+// ['10:00', '19:00']
+
+int freeslots(vector<pair<int,int>>p1,vector<pair<int,int>>p2)
+{
+	int m= p1.size();
+	for(int i=0;i<m-1;i++)
+	{
+		
+
+	}
+}
+
+int main()
+{
+	vector<pair<int,int>>p1;
+	vector<pair<int,int>>p2;
+	p1.push_back({9.5,10.5});
+	p1.push_back({12,14});
+	p1.push_back({15.5,17});
+	p1.push_back({18,18.5});
+
+
+	p2.push_back({10,10.5});
+	p2.push_back({11,13});
+	p2.push_back({});
+	p2.push_back({});
+
+}
